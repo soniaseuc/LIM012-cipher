51b45a95c6665345d7ba771e5a8c56c3101530a5
Sonia Seuc <60791273+soniaseuc@users.noreply.github.com>
2020-02-17 11:23:04 -0500

    primer sprint

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+//el numero de desplazamiento
+const clave = document.getElementById('desplazamiento');
+
+//para guardar en la variable textoDeEntradaACifrar todo lo que escriba el usuario
+const textoDeEntradaACifrar = document.getElementById('inputEncode');
+
+//la variable textoDeSalidaCifrado muestra toda la info que sale cifrada
+const textoDeSalidaCifrado = document.getElementById('outputEncode');
+
+//el boton de cifrado
+const botonDeCifrar = document.getElementById('cifrar');
+  botonDeCifrar.addEventListener('click',() => {
+    textoDeSalidaCifrado.value = cipher.encode (clave.value,textoDeEntradaACifrar.value);
+});
+//la variable textoDeSalidaCifrado muestra toda la info que sale cifrada
+const textoDeEntradaCifrado = document.getElementById('inputDecode');
+
+//la variable textoDescifrado muestra toda la info que sale descifrada
+const textoDescifrado = document.getElementById('outputDecode');
+
+//el boton de descifrado
+const botonDeDescifrar = document.getElementById('descifrar');
+  botonDeDescifrar.addEventListener('click',() => {
+    textoDescifrado.value = cipher.decode (clave.value,textoDeEntradaCifrado.value);
+});
