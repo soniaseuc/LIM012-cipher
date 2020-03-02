6a0590c210b98f4f8ff9db8f45868d1ea3972472
Sonia Seuc <60791273+soniaseuc@users.noreply.github.com>
2020-03-02 08:09:01 -0500

    presentacion del proyecto

--- src/cipher.js
+++ src/cipher.js
@@ -1,75 +1,74 @@
 const cipher = {
-  encode : (key,inputEncode) => {   
+  encode : (key,input) => {   
     let offset = parseInt(key); 
-    //console.log(typeof offset);
-    //console.log(typeof inputEncode);
+
     try {      
-      if(offset === 0 || inputEncode === "") throw new TypeError;
-      if(offset === 0 || inputEncode === " ") throw new TypeError;
-      if(offset === '' || inputEncode === "") throw  new TypeError;
-      if(offset === ' ' || inputEncode === " ") throw  new TypeError;
-      if(offset === null || typeof inputEncode === 'object') throw new TypeError;
-      if (offset === undefined || inputEncode === undefined) throw  new TypeError;      
+      if(offset === 0 || input === "") throw new TypeError;
+      if(offset === 0 || input === " ") throw new TypeError;
+      if(offset === '' || input === "") throw  new TypeError;
+      if(offset === ' ' || input === " ") throw  new TypeError;
+      if(offset === null || typeof input === 'object') throw new TypeError;
+      if (offset === undefined || input === undefined) throw  new TypeError;      
     }    
     catch(Err) {                 
       return Err();
     }    
     let myStringCipher = '';              
-    for(let i = 0; i< inputEncode.length; i++){  
-      let numberOfTheLetter = inputEncode.charCodeAt(i);
+    for(let i = 0; i< input.length; i++){  
+      let numberOfTheLetter = input.charCodeAt(i);
       if (numberOfTheLetter === 32){
         myStringCipher += ' ';
       } else if (numberOfTheLetter >= 65 && numberOfTheLetter <= 90) {                  
-          let asciiCode = (inputEncode.charCodeAt(i) - 65 + offset) % 26 + 65;
+          let asciiCode = (input.charCodeAt(i) - 65 + offset) % 26 + 65;
           let cipherLetter = String.fromCharCode(asciiCode);
           myStringCipher += cipherLetter;
       } else if (numberOfTheLetter >= 97 && numberOfTheLetter <= 122) {  
-          let asciiCode= (inputEncode.charCodeAt(i) - 97 + offset) % 26 + 97;
+          let asciiCode= (input.charCodeAt(i) - 97 + offset) % 26 + 97;
           let cipherLetter = String.fromCharCode(asciiCode);
           myStringCipher += cipherLetter;                         
       } else if (numberOfTheLetter >= 49 && numberOfTheLetter <= 57) {  
-          let asciiCode= (inputEncode.charCodeAt(i) - 49 + offset) % 26 + 49;
+          let asciiCode= (input.charCodeAt(i) - 49 + offset) % 9 + 49;
           let cipherLetter = String.fromCharCode(asciiCode);
           myStringCipher += cipherLetter;                         
       } else {
-         return 'Has insertado un caracter que no es válido';        
+        myStringCipher = myStringCipher + input[i];        
       }
     }       
     return myStringCipher;    
   },           
 
-  decode : (key,inputDecode) => {    
+  decode : (key,input) => {    
     let offset = parseInt(key);
     try {      
-      if(offset === 0 || inputDecode === "") throw new TypeError;
-      if(offset === 0 || inputDecode === " ") throw new TypeError;
-      if(offset === '' || inputDecode === "") throw new TypeError;
-      if(offset === ' ' || inputDecode === " ") throw new TypeError;
-      if(offset === null || typeof inputDecode === 'object') throw new TypeError;
-      if (offset === undefined || inputDecode === undefined) throw new TypeError;      
+      if(offset === 0 || input === "") throw new TypeError;
+      if(offset === 0 || input === " ") throw new TypeError;
+      if(offset === '' || input === "") throw new TypeError;
+      if(offset === ' ' || input === " ") throw new TypeError;
+      if(offset === null || typeof input === 'object') throw new TypeError;
+      if (offset === undefined || input === undefined) throw new TypeError;      
       }    
     catch(err) { 
       return err();
     }    
     let stringDecipher = '';// almacenar el valor del string descifrado              
-    for(let i = 0; i< inputDecode.length; i++){     
-      let numberOfTheLetter = inputDecode.charCodeAt(i);  
+    for(let i = 0; i< input.length; i++){     
+      let numberOfTheLetter = input.charCodeAt(i);  
       if (numberOfTheLetter === 32){
         stringDecipher += ' ';
       } else if (numberOfTheLetter >= 65 && numberOfTheLetter <= 90) {
-          let asciiCode = (inputDecode.charCodeAt(i) + 65 - offset) % 26 + 65;
+          let asciiCode = (input.charCodeAt(i) + 65 - offset) % 26 + 65;
           let decipherLetter = String.fromCharCode(asciiCode);
           stringDecipher += decipherLetter; // formar el string descifrado 
       } else if (numberOfTheLetter >= 97 && numberOfTheLetter <= 122) {
-          let asciiCode = (inputDecode.charCodeAt(i) - 45 - offset) % 26 + 97;
+          let asciiCode = (input.charCodeAt(i) - 45 - offset) % 26 + 97;
           let decipherLetter = String.fromCharCode(asciiCode);
           stringDecipher += decipherLetter; // formar el string descifrado
       } else if (numberOfTheLetter >= 48 && numberOfTheLetter <= 57) {  
-          let asciiCode= (inputDecode.charCodeAt(i) + 4 - offset) % 26 + 48;
+          let asciiCode= (input.charCodeAt(i) + 4 - offset) % 26 + 48;
           let decipherLetter = String.fromCharCode(asciiCode);
           stringDecipher += decipherLetter;                         
       } else {
-          return 'Has insertado un caracter que no es válido';         
+          stringDecipher = stringDecipher + input[i];         
       }
     }          
     return stringDecipher;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,22 @@
 import cipher from './cipher.js';
 
-//el numero de desplazamiento
+//el número de desplazamiento
 const key = document.getElementById('offset');
 
-//para guardar en la variable inputEncode todo lo que escriba el usuario
-const inputEncode = document.getElementById('input');
+//para guardar en la variable inputEncode todo lo que escriba el usuario para cifrar
+const input = document.getElementById('input');
 
 //la variable outputEncode muestra toda la info que sale cifrada
-const outputEncode = document.getElementById('output');
+const output = document.getElementById('output');
 
 //el boton de cifrado
 const btnCipher = document.getElementById('btnCipher');
   btnCipher.addEventListener('click',() => {    
-    outputEncode.value = cipher.encode (key.value,inputEncode.value);
+    output.value = cipher.encode (key.value,input.value);
 });
-//la variable inputDecode muestra toda la info que sale cifrada
-const inputDecode = document.getElementById('input');
-
-//la variable outputDecode muestra toda la info que sale descifrada
-const outputDecode = document.getElementById('output');
 
 //el boton de descifrado
 const btnDecipher = document.getElementById('btnDecipher');
   btnDecipher.addEventListener('click',() => {
-    outputDecode.value = cipher.decode (key.value,inputDecode.value);
+    output.value = cipher.decode (key.value,input.value);
 });
